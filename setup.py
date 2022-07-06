3ec641e07321267a450bdd33ac514e660b765649
Erik Kamph <erik.kamph38@gmail.com>
2022-07-06 13:16:03 +0200

    Updated gitignore and added a setup.py to the project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='menu',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Erik',
+    author_email='',
+    description='A menu program for commandline interfaces written using curses in python.'
+)
